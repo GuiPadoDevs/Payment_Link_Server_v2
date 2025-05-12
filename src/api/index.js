59790bd6873ab22719d2c54756bbe01028c42284
GuiPadoDevs <guilherme.padovine@wemov.com.br>
2025-05-12 13:34:42 -0300

    Add vercel.json

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 const app = require('../src/app');
+
 let isConnected = false;
 
 module.exports = async (req, res) => {
     if (!isConnected) {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            bufferCommands: false,
-        });
-        isConnected = true;
-        console.log('🔌 Conectado ao MongoDB (Vercel)');
+        try {
+            await mongoose.connect(process.env.MONGODB_URI, {
+                bufferCommands: false,
+            });
+            isConnected = true;
+            console.log('✅ MongoDB conectado na Vercel');
+        } catch (err) {
+            console.error('❌ Erro ao conectar no MongoDB', err);
+            return res.status(500).json({ error: 'Erro ao conectar no MongoDB' });
+        }
     }
 
     return app(req, res);
